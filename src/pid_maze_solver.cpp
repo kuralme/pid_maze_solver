62491b823440e77fdd30eef3f1e6356d633b9180
kuralme <kuralme@protonmail.com>
2025-06-19 15:13:44 +0000

    reverse maze follower implemented - rev sim waypoints yaml added

--- src/pid_maze_solver.cpp
+++ src/pid_maze_solver.cpp
@@ -1,7 +1,6 @@
 #include <Eigen/Dense>
 #include <array>
 #include <cmath>
-#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -10,8 +9,6 @@
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/impl/utils.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
 #include <yaml-cpp/yaml.h>
 
 using namespace std::chrono_literals;
@@ -33,9 +30,6 @@ public:
     timer_ = this->create_wall_timer(
         200ms, std::bind(&MazeSolver::executeCallback, this));
 
-    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
-    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
-
     SelectWaypoints();
     clock_ = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
   }
@@ -44,6 +38,7 @@ private:
   void SelectWaypoints() {
     // Waypoints [dx, dy, dphi] in robot frame
     switch (scene_number_) {
+
     case 1: { // Simulation
       std::string yaml_path =
           "/home/user/ros2_ws/src/pid_maze_solver/waypoints/waypoints_sim.yaml";
@@ -71,36 +66,48 @@ private:
       break;
     }
 
+    case 3: { // Simulation reverse
+      std::string yaml_path =
+          "/home/user/ros2_ws/src/pid_maze_solver/waypoints/"
+          "reverse_waypoints_sim.yaml";
+      YAML::Node config = YAML::LoadFile(yaml_path);
+      auto waypoints =
+          config["pid_maze_solver"]["ros__parameters"]["waypoints_sim"];
+      for (size_t i = 0; i < 14; ++i) {
+        waypoints_[i] = Eigen::Vector3f(waypoints[i * 3].as<float>(),
+                                        waypoints[i * 3 + 1].as<float>(),
+                                        waypoints[i * 3 + 2].as<float>());
+      }
+      break;
+    }
+    case 4: { // CyberWorld reverse
+      std::string yaml_path =
+          "/home/user/ros2_ws/src/pid_maze_solver/waypoints/"
+          "reverse_waypoints_real.yaml";
+      YAML::Node config = YAML::LoadFile(yaml_path);
+      auto waypoints =
+          config["pid_maze_solver"]["ros__parameters"]["waypoints_real"];
+      for (size_t i = 0; i < 14; ++i) {
+        waypoints_[i] = Eigen::Vector3f(waypoints[i * 3].as<float>(),
+                                        waypoints[i * 3 + 1].as<float>(),
+                                        waypoints[i * 3 + 2].as<float>());
+      }
+      break;
+    }
     default:
       RCLCPP_ERROR(this->get_logger(), "Invalid Scene Number: %d",
                    scene_number_);
     }
   }
 
-  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr /*msg*/) {
-    // Use tf2 to get transform from odom to base_link
-    geometry_msgs::msg::TransformStamped transformStamped;
-    try {
-      transformStamped =
-          tf_buffer_->lookupTransform("odom", "base_link", tf2::TimePointZero);
-
-      // Translation
-      current_pose_(0) = transformStamped.transform.translation.x;
-      current_pose_(1) = transformStamped.transform.translation.y;
-
-      // Yaw from quaternion
-      tf2::Quaternion q(transformStamped.transform.rotation.x,
-                        transformStamped.transform.rotation.y,
-                        transformStamped.transform.rotation.z,
-                        transformStamped.transform.rotation.w);
-      current_pose_(2) = tf2::impl::getYaw(q);
-
-      got_odom_ = true;
-    } catch (tf2::TransformException &ex) {
-      RCLCPP_WARN(this->get_logger(), "Could not transform odom->base_link: %s",
-                  ex.what());
-      got_odom_ = false;
-    }
+  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+    auto orientation = msg->pose.pose.orientation;
+    tf2::Quaternion q(orientation.x, orientation.y, orientation.z,
+                      orientation.w);
+    current_pose_(0) = msg->pose.pose.position.x;
+    current_pose_(1) = msg->pose.pose.position.y;
+    current_pose_(2) = tf2::impl::getYaw(q);
+    got_odom_ = true;
   }
 
   void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
@@ -133,6 +140,7 @@ private:
     // Update target waypoint
     if (wp_reached_ || init_) {
       target_pose_ = current_pose_ + waypoints_[target_wp_];
+
       wp_reached_ = false;
       init_ = false;
       size_t twp = target_wp_ + 1;
@@ -141,7 +149,6 @@ private:
 
     // Error vector
     Eigen::Vector3f error_pose = target_pose_ - current_pose_;
-    wp_reached_ = false;
     if (error_pose(2) > M_PI) {
       error_pose(2) -= 2 * M_PI;
     } else if (error_pose(2) < -M_PI) {
@@ -152,6 +159,11 @@ private:
     if (std::hypot(error_pose(0), error_pose(1)) < 0.02) {
       auto msg = geometry_msgs::msg::Twist();
       twist_pub_->publish(msg);
+
+      // Perform wall avoidance
+      if (performWallAvoidance()) {
+        return;
+      }
 
       if (std::abs(error_pose(2)) > 0.02) {
         // PID Controller - Angular
@@ -175,7 +187,12 @@ private:
         target_wp_++;
 
         if (target_wp_ >= waypoints_.size()) {
-          RCLCPP_INFO(this->get_logger(), "Maze finished!");
+          if (scene_number_ == 1 || scene_number_ == 2)
+            RCLCPP_INFO(this->get_logger(), "Maze finished!");
+          else if (scene_number_ == 3 || scene_number_ == 4) {
+            RCLCPP_INFO(this->get_logger(), "Reverse-Maze finished!");
+          }
+
           rclcpp::shutdown();
         } else {
           // Start the pause timer
@@ -198,21 +215,16 @@ private:
     Eigen::Vector3f V = Kp_ * error_pose + Kd_ * (error_pose - prev_error_) +
                         Ki_ * integral_error_;
     prev_error_ = error_pose;
-
-    // Transpose velocity command vector into robot frame
     V = recomputeTwist(V);
 
-    // Apply simple obstacle avoidance
-    // if (std::abs(V(0)) > 0.01 || std::abs(V(1)) > 0.01) {
-    V = performObstacleAvoidance(V);
-    // }
+    // Apply course correction
+    V = performCourseCorrection(V);
 
     // Publish linear velocities
     auto cmd_vel = geometry_msgs::msg::Twist();
     cmd_vel.linear.x = std::clamp(V(0), -max_lin_vel_, max_lin_vel_);
     cmd_vel.linear.y = std::clamp(V(1), -max_lin_vel_, max_lin_vel_);
     cmd_vel.angular.z = 0.0;
-
     twist_pub_->publish(cmd_vel);
   }
 
@@ -222,70 +234,83 @@ private:
     Eigen::Matrix3f R{{std::cos(dphi), std::sin(dphi), 0.0},
                       {-std::sin(dphi), std::cos(dphi), 0.0},
                       {0.0, 0.0, 1.0}};
+    // Velocity command vector transposed into robot frame
     Eigen::Vector3f nu = R * V;
     return nu;
   }
 
-  Eigen::Vector3f performObstacleAvoidance(const Eigen::Vector3f &cmd_vel) {
+  bool performWallAvoidance() {
 
-    const float front = laser_ranges_[0];
-    const float left = laser_ranges_[179];
-    const float back = laser_ranges_[359];
-    const float right = laser_ranges_[579];
+    float front = laser_ranges_[0];
+    float back = laser_ranges_[459];
+    float left = laser_ranges_[229];
+    float right = laser_ranges_[689];
 
+    auto cmd_vel = geometry_msgs::msg::Twist();
+    bool avoided = false;
+
+    // Thresholds
+    const float min_dist = 0.2;
+    const float backoff_vel = 0.1;
+
+    if (front < min_dist) {
+      cmd_vel.linear.x = -backoff_vel;
+      avoided = true;
+    } else if (back < min_dist) {
+      cmd_vel.linear.x = backoff_vel;
+      avoided = true;
+    }
+
+    if (left < min_dist) {
+      cmd_vel.linear.y = -backoff_vel;
+      avoided = true;
+    } else if (right < min_dist) {
+      cmd_vel.linear.y = backoff_vel;
+      avoided = true;
+    }
+
+    if (avoided) {
+      RCLCPP_WARN(this->get_logger(), "Wall too close. Avoiding...");
+      twist_pub_->publish(cmd_vel);
+      return true;
+    }
+
+    return false;
+  }
+
+  Eigen::Vector3f performCourseCorrection(const Eigen::Vector3f &cmd_vel) {
+
+    float left = laser_ranges_[229];
+    float right = laser_ranges_[689];
+
+    const float critical_dist = 0.15;
     const float min_valid = 0.05;
     const float max_valid = 5.0;
-    const float critical_dist = 0.21;
+
+    Eigen::Vector3f vel_updt = cmd_vel;
 
     bool left_valid = (left > min_valid && left < max_valid);
     bool right_valid = (right > min_valid && right < max_valid);
-    bool back_valid = (back > min_valid && back < max_valid);
-    bool front_valid = (front > min_valid && front < max_valid);
 
-    Eigen::Vector3f vel_update = cmd_vel;
-    Eigen::Vector2f target_correction = Eigen::Vector2f::Zero();
-
-    // Perform course correction
-    if (left_valid && left < critical_dist) {
-      RCLCPP_WARN(this->get_logger(), "Course correcting to right...");
-      vel_update(1) -= 0.05;
-      target_correction(1) -= 0.003; // update target to right
-
-    } else if (right_valid && right < critical_dist - 0.01) {
-      RCLCPP_WARN(this->get_logger(), "Course correcting to left...");
-      vel_update(1) += 0.05;
-      target_correction(1) += 0.003; // update target to left
+    // Apply only if moving linearly
+    if (std::abs(cmd_vel(0)) > 0.01 || std::abs(cmd_vel(1)) > 0.01) {
+      if (left_valid && left < critical_dist) {
+        vel_updt(1) += 0.05;
+      } else if (right_valid && right < critical_dist) {
+        vel_updt(1) -= 0.05;
+      } else if (left_valid && right_valid) {
+        float drift_error = right - left;
+        if (std::abs(drift_error) > 0.15) {
+          vel_updt(1) += -0.01 * (drift_error > 0 ? 1 : -1);
+        }
+      }
     }
-
-    // Perform wall avoidance
-    if (front_valid && front < critical_dist - 0.01) {
-      RCLCPP_WARN(this->get_logger(), "Avoiding front wall...");
-      vel_update(0) = -0.05;
-      target_correction(0) -= 0.003; // update target to back
-
-    } else if (back_valid && back < critical_dist + 0.02) {
-      RCLCPP_WARN(this->get_logger(), "Avoiding back wall...");
-      vel_update(0) = 0.05;
-      target_correction(0) += 0.003; // update target to front
-    }
-
-    // Transform corrections
-    if (target_correction.norm() > 0.0) {
-      const float dphi = current_pose_(2);
-      Eigen::Matrix2f R{{std::cos(dphi), std::sin(dphi)},
-                        {-std::sin(dphi), std::cos(dphi)}};
-      Eigen::Vector2f tranformed_target = R * target_correction;
-      target_pose_.head<2>() += tranformed_target;
-    }
-
-    return vel_update;
+    return vel_updt;
   }
 
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
-  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Time pause_time_;
   int scene_number_;
@@ -300,10 +325,10 @@ private:
   std::vector<float> laser_ranges_;
 
   // PID Gains
-  const float Kp_ = 0.6;
-  const float Ki_ = 0.005, int_limit_ = 5.0;
-  const float Kd_ = 0.35;
-  const float max_lin_vel_ = 0.25;
+  const float Kp_ = 1.2;
+  const float Ki_ = 0.02, int_limit_ = 5.0;
+  const float Kd_ = 0.8;
+  const float max_lin_vel_ = 0.5;
   const float max_ang_vel_ = 0.8;
 };
 
@@ -311,7 +336,7 @@ int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
   // Check if a scene number argument is provided
-  int scene_number = 2; // Default scene number to cyberworld
+  int scene_number = 3; // Default scene number to simulation
   if (argc > 1) {
     scene_number = std::atoi(argv[1]);
   }
